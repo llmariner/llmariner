ee3335399b64dd0f1f0923d4530255c381496ad4
Guangrui Fu <guangrui@cloudnatix.com>
2024-04-11 23:29:20 +0000

    setup rag using chroma

---
+++ hack/ollama_rag/README.md
@@ -0,0 +1,26 @@
+# Run OLLAMA with RAG in KIND cluster
+
+This is an example on setting up RAG using Chroma vector database and serving the model using ollama.
+
+## Build docker
+
+```console
+docker build -t rag:latest -f ./Dockerfile.chroma .
+kind load docker-image rag:latest --name <your-kind-cluster-name>
+```
+
+## Deploy OLLAMA
+
+To deploy OLLAMA in a k8s cluster with GPU enabled: 
+
+```console
+kubectl apply -f ./ollama_gpu.yaml 
+```
+
+## Run RAG
+
+```console
+kubectl exec -it -n ollama <ollama-pod-name> -- python3 /tmp/rag_chroma.py
+```
+
+Note: This example uses gemma:2b as LLM model, but the RAG can be applied to other LLM model as well, e.g. mistral. 
---
+++ hack/ollama_rag/rag_chroma.py
@@ -0,0 +1,65 @@
+from langchain_community.vectorstores import Chroma
+from langchain_community.chat_models import ChatOllama
+from langchain_community.embeddings import FastEmbedEmbeddings
+from langchain.schema.output_parser import StrOutputParser
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores.utils import filter_complex_metadata
+
+
+class ChatPDF:
+    vector_store = None
+    retriever = None
+    chain = None
+
+    def __init__(self):
+        self.model = ChatOllama(model="gemma:2b")
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
+        self.prompt = PromptTemplate.from_template(
+            """
+            <s> [INST] Vous êtes un assistant pour les tâches de réponse aux questions. Utilisez les éléments de contexte suivants pour répondre à la question.
+            Si vous ne connaissez pas la réponse, dites simplement que vous ne savez pas.. Utilisez trois phrases
+             maximum et soyez concis dans votre réponse. [/INST] </s>
+            [INST] Question: {question}
+            Context: {context}
+            Answer: [/INST]
+            """
+        )
+
+    def ingest(self, pdf_file_path: str):
+        docs = PyPDFLoader(file_path=pdf_file_path).load()
+        chunks = self.text_splitter.split_documents(docs)
+        chunks = filter_complex_metadata(chunks)
+
+        vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+        self.retriever = vector_store.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={
+                "k": 3,
+                "score_threshold": 0.5,
+            },
+        )
+
+        self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+                      | self.prompt
+                      | self.model
+                      | StrOutputParser())
+
+    def ask(self, query: str):
+        if not self.chain:
+            return "Please, add a PDF document first."
+
+        return self.chain.invoke(query)
+
+    def clear(self):
+        self.vector_store = None
+        self.retriever = None
+        self.chain = None
+
+if __name__ == "__main__":
+    s = ChatPDF()
+    s.ingest("/home/src/SchoolProfile2023-2024.pdf")
+    output = s.ask("What classes are offered at Gunn")
+    print(output)
