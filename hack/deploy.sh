ba33836109c56e97c7c2d46eaf33d4787ed4ccf3
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-11 10:34:26 -0700

    Add file-manager integration with MinIO (#39)

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-./deploy_kong.sh
-
 cluster_name="llm-operator-demo"
 
 model_manager_repo="../../model-manager"
@@ -11,22 +9,23 @@ file_manager_repo="../../file-manager"
 inference_manager_repo="../../inference-manager"
 job_manager_repo="../../job-manager"
 
-
 kubectl create namespace postgres
-kubectl create namespace minio
 kubectl create namespace model-manager
 kubectl create namespace file-manager
 kubectl create namespace inference-manager
 kubectl create namespace job-manager
 
 kubectl apply --namespace postgres -f postgres.yaml
-
-kubectl apply --namespace minio -f minio.yaml
-
-# TODO(kenji): Run this after the postgres pod starts running.
+kubectl wait --for=condition=ready pod -n postgres -l app=postgres
+# Wait for extra seconds
+sleep 5
 kubectl exec  -n postgres deploy/postgres -- psql -h localhost -U ps_user --no-password -p 5432 -d ps_db -c "CREATE DATABASE model_manager;"
 kubectl exec  -n postgres deploy/postgres -- psql -h localhost -U ps_user --no-password -p 5432 -d ps_db -c "CREATE DATABASE file_manager;"
 kubectl exec  -n postgres deploy/postgres -- psql -h localhost -U ps_user --no-password -p 5432 -d ps_db -c "CREATE DATABASE job_manager;"
+
+./deploy_kong.sh
+
+./deploy_minio.sh
 
 kubectl apply -n model-manager -f postgres-secret.yaml
 kubectl apply -n file-manager -f postgres-secret.yaml
