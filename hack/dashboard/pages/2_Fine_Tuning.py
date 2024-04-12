1f7d2761730a48dc0aa334370e1a58b79dbbbb1b
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-12 11:29:11 -0700

    Make a minor style change

--- hack/dashboard/pages/2_Fine_Tuning.py
+++ hack/dashboard/pages/2_Fine_Tuning.py
@@ -5,9 +5,26 @@ import datetime
 
 st.markdown(
     """
-# Fine Tuning
+# Fine Tuning Dashboard
 """
 )
+
+# style
+th_props = [
+  ('font-size', '18px'),
+  ('text-align', 'center'),
+  ('font-weight', 'bold'),
+  ('color', '#6d6d6d'),
+]
+
+td_props = [
+  ('font-size', '18px')
+]
+
+styles = [
+  dict(selector="th", props=th_props),
+  dict(selector="td", props=td_props)
+]
 
 client = OpenAI(
   base_url="http://localhost:80/v1",
@@ -23,7 +40,7 @@ for m in models.data:
     modelsData['ID'].append(m.id)
     modelsData['Created at'].append(datetime.datetime.fromtimestamp(int(m.created)))
 st.subheader("Models")
-st.table(pd.DataFrame(modelsData))
+st.table(pd.DataFrame(modelsData).style.set_properties(**{'text-align': 'left'}).set_table_styles(styles))
 
 files = client.files.list()
 filesData = {
@@ -36,7 +53,7 @@ for f in files.data:
     filesData['Filename'].append(f.filename)
     filesData['Created at'].append(datetime.datetime.fromtimestamp(int(f.createdAt)))
 st.subheader("Files")
-st.write(pd.DataFrame(filesData))
+st.table(pd.DataFrame(filesData).style.set_properties(**{'text-align': 'left'}).set_table_styles(styles))
 
 jobs = client.fine_tuning.jobs.list()
 jobsData = {
@@ -47,4 +64,4 @@ for j in jobs.data:
     jobsData['ID'].append(j.id)
     jobsData['Created at'].append(datetime.datetime.fromtimestamp(int(j.createdAt)))
 st.subheader("Jobs")
-st.write(pd.DataFrame(jobsData))
+st.table(pd.DataFrame(jobsData).style.set_properties(**{'text-align': 'left'}).set_table_styles(styles))
