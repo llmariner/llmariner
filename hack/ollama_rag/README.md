4d60f6f726bd0612031d63d9e9903ecafa86a50d
G. Fu <62898123+guangrui-cloudnatix@users.noreply.github.com>
2024-04-16 11:57:27 -0700

    setup rag using milvus (#46)

--- hack/ollama_rag/README.md
+++ hack/ollama_rag/README.md
@@ -1,15 +1,15 @@
 # Run OLLAMA with RAG in KIND cluster
 
-This is an example on setting up RAG using Chroma vector database and serving the model using ollama.
+## This is an example on setting up RAG using Chroma vector database and serving the model using ollama.
 
-## Build docker
+### Build docker
 
 ```console
 docker build -t rag:latest -f ./Dockerfile.chroma .
 kind load docker-image rag:latest --name <your-kind-cluster-name>
 ```
 
-## Deploy OLLAMA
+### Deploy OLLAMA
 
 To deploy OLLAMA in a k8s cluster with GPU enabled: 
 
@@ -17,10 +17,40 @@ To deploy OLLAMA in a k8s cluster with GPU enabled:
 kubectl apply -f ./ollama_gpu.yaml 
 ```
 
-## Run RAG
+### Run RAG
 
 ```console
 kubectl exec -it -n ollama <ollama-pod-name> -- python3 /tmp/rag_chroma.py
+```
+
+## This is an example on setting up RAG using Milvus vector database and serving the model using ollama.
+
+### Install Milvus
+
+```console
+helm repo add milvus https://zilliztech.github.io/milvus-helm/
+helm repo update
+helm upgrade --install -f ./milvus_values.yaml milvus milvus/milvus --namespace milvus --create-namespace
+```
+### Build docker
+
+```console
+docker build -t rag:latest -f ./Dockerfile.milvus .
+kind load docker-image rag:latest --name <your-kind-cluster-name>
+```
+
+### Deploy OLLAMA
+
+To deploy OLLAMA in a k8s cluster with GPU enabled: 
+
+```console
+kubectl apply -f ./ollama_gpu.yaml 
+```
+
+### Run RAG
+
+```console
+kubectl exec -it -n ollama <ollama-pod-name> -- python3 /home/src//rag_milvus.py
 ```
 
 Note: This example uses gemma:2b as LLM model, but the RAG can be applied to other LLM model as well, e.g. mistral. 
--- hack/ollama_rag/rag_milvus.py
+++ hack/ollama_rag/rag_milvus.py
@@ -1,12 +1,17 @@
-from langchain_community.vectorstores import Chroma
+# inspired from https://python.langchain.com/docs/integrations/vectorstores/milvus/
+
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Milvus
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+
 from langchain_community.chat_models import ChatOllama
-from langchain_community.embeddings import FastEmbedEmbeddings
 from langchain.schema.output_parser import StrOutputParser
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.vectorstores.utils import filter_complex_metadata
 from langchain import hub
+
 
 class ChatPDF:
     vector_store = None
@@ -15,7 +20,7 @@ class ChatPDF:
 
     def __init__(self):
         self.model = ChatOllama(model="gemma:2b")
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
+        self.text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         self.prompt = hub.pull("rlm/rag-prompt-llama")
 
     def ingest(self, pdf_file_path: str):
@@ -23,13 +28,26 @@ class ChatPDF:
         chunks = self.text_splitter.split_documents(docs)
         chunks = filter_complex_metadata(chunks)
 
-        vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
+        embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        COLLECTION_NAME = 'rag_db'
+        URI = 'http://milvus.default.svc.cluster.local:19530'
+        connection_args = { 'uri': URI }
+        
+        vector_store = Milvus(
+            embedding_function=embeddings,
+            collection_name=COLLECTION_NAME,
+            connection_args=connection_args,
+            drop_old=True,
+        ).from_documents(
+            chunks,
+            embedding=embeddings,
+            collection_name=COLLECTION_NAME,
+            connection_args=connection_args,
+        )
+
         self.retriever = vector_store.as_retriever(
-            search_type="similarity_score_threshold",
-            search_kwargs={
-                "k": 3,
-                "score_threshold": 0.5,
-            },
+           search_type="mmr", 
+           search_kwargs={"k": 1}
         )
 
         self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
