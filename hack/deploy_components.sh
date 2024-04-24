19c77cb148f4f3e5f02cbd45d5633908cd44a53a
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-23 22:34:09 -0700

    Deploy with the llm-operator helm chart

--- hack/deploy_components.sh
+++ hack/deploy_components.sh
@@ -2,49 +2,12 @@
 
 set -euo pipefail
 
-llm_operator_namespace=llm-operator
-
 helm repo add llm-operator http://llm-operator-charts.s3-website-us-west-2.amazonaws.com/
 helm repo update
 
 helm upgrade \
   --install \
-  -n "${llm_operator_namespace}" \
-  model-manager-server \
-  llm-operator/model-manager-server \
-  -f model-manager-server-values.yaml
-
-helm upgrade \
-  --install \
-  -n "${llm_operator_namespace}" \
-  model-manager-loader \
-  llm-operator/model-manager-loader \
-  -f model-manager-loader-values.yaml
-
-helm upgrade \
-  --install \
-  -n "${llm_operator_namespace}" \
-  file-manager-server \
-  llm-operator/file-manager-server \
-  -f file-manager-server-values.yaml
-
-helm upgrade \
-  --install \
-  -n "${llm_operator_namespace}" \
-  inference-manager-engine \
-  llm-operator/inference-manager-engine \
-  -f inference-manager-engine-values.yaml
-
-helm upgrade \
-  --install \
-  -n "${llm_operator_namespace}" \
-  job-manager-server \
-  llm-operator/job-manager-server \
-  -f job-manager-server-values.yaml
-
-helm upgrade \
-  --install \
-  -n "${llm_operator_namespace}" \
-  job-manager-dispatcher \
-  llm-operator/job-manager-dispatcher \
-  -f job-manager-dispatcher-values.yaml
+  -n llm-operator \
+  llm-operator \
+  llm-operator/llm-operator \
+  -f llm-operator-values.yaml
