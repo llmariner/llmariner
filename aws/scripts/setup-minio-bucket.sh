055d5a8b64140bab81797fde5076f39facdb9b63
Jun Mukai <86621109+junm-cloudnatix@users.noreply.github.com>
2024-04-25 16:16:40 -0700

    Add demo llm-operator setup. (#92)
    This is analogous to hack/deploy.sh but using ansible functionalities.

---
+++ aws/scripts/setup-minio-bucket.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+trap 'kill $(jobs -p)' EXIT
+
+kubectl port-forward -n minio service/minio 9000 9090 &
+sleep 5
+
+minio_user=minioadmin
+minio_password=minioadmin
+
+# Obtain the cookie and store in cookies.txt.
+curl \
+  http://localhost:9090/api/v1/login \
+  --cookie-jar cookies.txt \
+  --request POST \
+  --header 'Content-Type: application/json' \
+  --data "{\"accessKey\": \"${minio_user}\", \"secretKey\": \"${minio_password}\"}"
+
+export AWS_ACCESS_KEY_ID=llm-operator-key
+export AWS_SECRET_ACCESS_KEY=llm-operator-secret
+
+# Create a new API key.
+curl \
+  http://localhost:9090/api/v1/service-account-credentials \
+  --cookie cookies.txt \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data "{\"policy\": \"\", \"accessKey\": \"${AWS_ACCESS_KEY_ID}\", \"secretKey\": \"${AWS_SECRET_ACCESS_KEY}\", \"description\": \"\", \"comment\": \"\", \"name\": \"LLM Operator\", \"expiry\": null}"
+
+rm cookies.txt
+
+# Create a new bucket.
+bucket_name=llm-operator
+aws --endpoint-url http://localhost:9000 s3 mb s3://${bucket_name}
+
+# Create secrets.
+kubectl create secret generic -n llm-operator aws \
+  --from-literal=accessKeyId=${AWS_ACCESS_KEY_ID} \
+  --from-literal=secretAccessKey=${AWS_SECRET_ACCESS_KEY}
