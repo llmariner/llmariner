7ecf38a2c2b91a3b0abb1b88884bedbed8d96a31
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-03 22:26:38 -0700

    Create a database for user-manager (#126)

--- hack/deploy_postgres.sh
+++ hack/deploy_postgres.sh
@@ -8,7 +8,7 @@ kubectl wait --timeout=60s --for=condition=ready pod -n postgres -l app=postgres
 # Wait for extra seconds
 sleep 5
 
-dbs=("model_manager" "file_manager" "job_manager" "dex")
+dbs=("model_manager" "file_manager" "job_manager" "user_manager" "dex")
 for db in "${dbs[@]}"; do
   kubectl exec  -n postgres deploy/postgres -- psql -h localhost -U ps_user --no-password -p 5432 -d ps_db -c "CREATE DATABASE ${db};"
 done
