65d1e0cf68b5e8266ea12fb1f7677cf8c907fe2c
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-07 14:53:49 -0700

    Fix bugs in check_readiness.sh

--- hack/check_readiness.sh
+++ hack/check_readiness.sh
@@ -14,12 +14,15 @@ paths=(
 for path in "${paths[@]}"; do
   curl --fail --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" "${LLM_ENDPOINT_URL}/${path}" > /dev/null
 done
-curl --request POST --fail --silent "http://${LLM_ENDPOINT_URL}/chat/completions" -d '{
+
+echo "Checking if base models are loaded."
+curl --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" "${LLM_ENDPOINT_URL}/basemodels" | jq -e '.data | map(select(.id == "google-gemma-2b-it-q4")) | length == 1' > /dev/null
+
+echo "Checking if chat completions work."
+curl --request POST --fail --silent "${LLM_ENDPOINT_URL}/chat/completions" -d '{
   "model": "google-gemma-2b-it-q4",
   "messages": [{"role": "user", "content": "Why is the sky blue?"}]
 }' > /dev/null
 
-echo "Checking if base models are loaded."
-curl --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" "${LLM_ENDPOINT_URL}/v1/basemodels" | jq -e '.data | map(select(.id == "google/gemma-2b")) | length == 1' > /dev/null
 
 echo "Passed."
