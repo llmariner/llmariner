cc44c584d2d2591a8ee320677fb08b24d2585748
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-07 14:40:10 -0700

    Make endpoint URL configurable in check_readiness.sh

--- hack/check_readiness.sh
+++ hack/check_readiness.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+LLM_ENDPOINT_URL="${LLM_ENDPOINT_URL:-http://localhost/v1}"
+
 echo "Checking if API endpoints are reachable."
 paths=(
   basemodels
@@ -10,14 +12,14 @@ paths=(
   models
 )
 for path in "${paths[@]}"; do
-  curl --fail --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" "http://localhost/v1/${path}" > /dev/null
+  curl --fail --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" "${LLM_ENDPOINT_URL}/${path}" > /dev/null
 done
-curl --request POST --fail --silent "http://localhost/v1/chat/completions" -d '{
+curl --request POST --fail --silent "http://${LLM_ENDPOINT_URL}/chat/completions" -d '{
   "model": "google-gemma-2b-it-q4",
   "messages": [{"role": "user", "content": "Why is the sky blue?"}]
 }' > /dev/null
 
 echo "Checking if base models are loaded."
-curl --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" http://localhost/v1/basemodels | jq -e '.data | map(select(.id == "google/gemma-2b")) | length == 1' > /dev/null
+curl --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" "${LLM_ENDPOINT_URL}/v1/basemodels" | jq -e '.data | map(select(.id == "google/gemma-2b")) | length == 1' > /dev/null
 
 echo "Passed."
