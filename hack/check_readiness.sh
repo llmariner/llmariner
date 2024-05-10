ab3d3ffeebf95104be5990f609f68038c4d85091
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-10 16:28:54 -0700

    Change env vars in check_readiness.sh to follow OpenAI

--- hack/check_readiness.sh
+++ hack/check_readiness.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-LLM_ENDPOINT_URL="${LLM_ENDPOINT_URL:-http://localhost/v1}"
+OPENAI_BASE_URL="${OPENAI_BASE_URL:-http://localhost/v1}"
 
 echo "Checking if API endpoints are reachable."
 paths=(
@@ -12,14 +12,14 @@ paths=(
   models
 )
 for path in "${paths[@]}"; do
-  curl --fail --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" "${LLM_ENDPOINT_URL}/${path}" > /dev/null
+  curl --fail --silent -H "Authorization: Bearer ${OPENAI_API_KEY}" "${OPENAI_BASE_URL}/${path}" > /dev/null
 done
 
 echo "Checking if base models are loaded."
-curl --silent -H "Authorization: Bearer ${LLM_OPERATOR_TOKEN}" "${LLM_ENDPOINT_URL}/basemodels" | jq -e '.data | map(select(.id == "google-gemma-2b-it-q4")) | length == 1' > /dev/null
+curl --silent -H "Authorization: Bearer ${OPENAI_API_KEY}" "${OPENAI_BASE_URL}/basemodels" | jq -e '.data | map(select(.id == "google-gemma-2b-it-q4")) | length == 1' > /dev/null
 
 echo "Checking if chat completions work."
-curl --request POST --fail --silent "${LLM_ENDPOINT_URL}/chat/completions" -d '{
+curl --request POST --fail --silent "${OPENAI_BASE_URL}/chat/completions" -d '{
   "model": "google-gemma-2b-it-q4",
   "messages": [{"role": "user", "content": "Why is the sky blue?"}]
 }' > /dev/null
