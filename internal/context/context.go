bdca9e6f93d7893c7928fbd824247c5ad7e28985
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-24 23:00:57 -0700

    Change 'context set' to choose org and project from a list (#51)

--- internal/context/context.go
+++ internal/context/context.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/llm-operator/cli/internal/auth/org"
 	"github.com/llm-operator/cli/internal/auth/project"
 	"github.com/llm-operator/cli/internal/runtime"
+	"github.com/llm-operator/cli/internal/ui"
 	uv1 "github.com/llm-operator/user-manager/api/v1"
 	"github.com/spf13/cobra"
 )
@@ -32,135 +34,189 @@ func Cmd() *cobra.Command {
 
 func getCmd() *cobra.Command {
 	return &cobra.Command{
-		Use: "get",
+		Use:  "get",
+		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return get(cmd.Context(), args)
+			return get(cmd.Context())
 		},
 	}
 }
 
 func setCmd() *cobra.Command {
 	return &cobra.Command{
-		Use: "set",
+		Use:  "set",
+		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(args) != 2 {
-				return fmt.Errorf("expected <key> <value>")
-			}
-			return set(cmd.Context(), args[0], args[1])
+			return set(cmd.Context())
 		},
 	}
 }
 
-func get(ctx context.Context, args []string) error {
+func get(ctx context.Context) error {
 	env, err := runtime.NewEnv(ctx)
 	if err != nil {
 		return err
 	}
 
-	var keys []string
-	if len(args) == 0 {
-		keys = append(keys, orgKey, projectKey)
-	} else {
-		keys = append(keys, args[0])
+	oid := env.Config.Context.OrganizationID
+	if oid == "" {
+		fmt.Printf("Organization:\n  (not selected)\n")
+		return nil
 	}
 
-	for _, key := range keys {
-		switch key {
-		case orgKey, organizationKey:
-			oid := env.Config.Context.OrganizationID
-			if oid == "" {
-				fmt.Printf("Organization:\n  (not selected)\n")
-			} else {
-				orgs, err := org.ListOrganizations(env)
-				if err != nil {
-					return err
-				}
-				var found *uv1.Organization
-				for _, o := range orgs {
-					if o.Id == oid {
-						found = o
-						break
-					}
-				}
-				if found == nil {
-					return fmt.Errorf("org %q not found", oid)
-				}
-				fmt.Printf("Organization:\n  Title: %q\n  ID: %q\n", found.Title, oid)
-			}
-		case projectKey:
-			pid := env.Config.Context.ProjectID
-			if pid == "" {
-				fmt.Printf("Project:\n  (not selected)\n")
-			} else {
-				ps, err := project.ListProjects(env, "")
-				if err != nil {
-					return err
-				}
-				var found *uv1.Project
-				for _, p := range ps {
-					if p.Id == pid {
-						found = p
-						break
-					}
-				}
+	orgs, err := org.ListOrganizations(env)
+	if err != nil {
+		return err
+	}
+	foundO, err := findOrgByID(orgs, oid)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Organization:\n  Title: %q\n  ID: %q\n", foundO.Title, oid)
 
-				if found == nil {
-					return fmt.Errorf("project not found")
-				}
-				fmt.Printf("Project:\n  Title: %q\n  ID: %q\n", found.Title, pid)
-			}
-		default:
-			return fmt.Errorf("unknown key %s", key)
+	pid := env.Config.Context.ProjectID
+	if pid == "" {
+		fmt.Printf("Project:\n  (not selected)\n")
+		return nil
+	}
+
+	ps, err := project.ListProjects(env, foundO.Title)
+	if err != nil {
+		return err
+	}
+	foundP, err := findProjectByID(ps, pid)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Project:\n  Title: %q\n  ID: %q\n", foundP.Title, pid)
+
+	return nil
+}
+
+func set(ctx context.Context) error {
+	p := ui.NewPrompter()
+
+	env, err := runtime.NewEnv(ctx)
+	if err != nil {
+		return err
+	}
+
+	orgs, err := org.ListOrganizations(env)
+	if err != nil {
+		return err
+	}
+	defaultTitle := orgs[0].Title
+	if id := env.Config.Context.OrganizationID; id != "" {
+		o, err := findOrgByID(orgs, id)
+		if err != nil {
+			return err
 		}
+		defaultTitle = o.Title
+	}
+
+	var orgTitles []string
+	for _, o := range orgs {
+		orgTitles = append(orgTitles, o.Title)
+	}
+
+	var selectedTitle string
+	if err := p.Ask(&survey.Select{
+		Message: "Organization",
+		Options: orgTitles,
+		Default: defaultTitle,
+	}, &selectedTitle); err != nil {
+		return fmt.Errorf("failed to select org: %s", err)
+	}
+
+	selectedO, err := findOrgByTitle(orgs, selectedTitle)
+	if err != nil {
+		return err
+	}
+
+	orgChanged := env.Config.Context.OrganizationID != selectedO.Id
+	env.Config.Context.OrganizationID = selectedO.Id
+
+	projects, err := project.ListProjects(env, selectedO.Title)
+	if err != nil {
+		return err
+	}
+
+	if len(projects) == 0 {
+		fmt.Println("No projects found. Only setting organization.")
+		env.Config.Context.ProjectID = ""
+		if err := env.Config.Save(); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	defaultTitle = projects[0].Title
+	if id := env.Config.Context.ProjectID; id != "" && !orgChanged {
+		o, err := findOrgByID(orgs, id)
+		if err != nil {
+			return err
+		}
+		defaultTitle = o.Title
+	}
+
+	var projectTitles []string
+	for _, p := range projects {
+		projectTitles = append(projectTitles, p.Title)
+	}
+
+	if err := p.Ask(&survey.Select{
+		Message: "Project",
+		Options: projectTitles,
+		Default: defaultTitle,
+	}, &selectedTitle); err != nil {
+		return fmt.Errorf("failed to select project: %s", err)
+	}
+
+	selectedP, err := findProjectByTitle(projects, selectedTitle)
+	if err != nil {
+		return err
+	}
+	env.Config.Context.ProjectID = selectedP.Id
+
+	if err := env.Config.Save(); err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func set(ctx context.Context, key, value string) error {
-	env, err := runtime.NewEnv(ctx)
-	if err != nil {
-		return err
+func findOrgByID(orgs []*uv1.Organization, id string) (*uv1.Organization, error) {
+	for _, o := range orgs {
+		if o.Id == id {
+			return o, nil
+		}
 	}
-
-	switch key {
-	case orgKey, organizationKey:
-		o, found, err := org.FindOrgByTitle(env, value)
-		if err != nil {
-			return err
+	return nil, fmt.Errorf("org of ID %q not found", id)
+}
+func findOrgByTitle(orgs []*uv1.Organization, title string) (*uv1.Organization, error) {
+	for _, o := range orgs {
+		if o.Title == title {
+			return o, nil
 		}
-		if !found {
-			return fmt.Errorf("organization not found")
-		}
-		if o.Id == env.Config.Context.OrganizationID {
-			// Do nothing
-			return nil
-		}
-		env.Config.Context.OrganizationID = o.Id
-		// Clear the project.
-		env.Config.Context.ProjectID = ""
-		if err := env.Config.Save(); err != nil {
-			return err
-		}
-	case projectKey:
-		if env.Config.Context.OrganizationID == "" {
-			return fmt.Errorf("organization must be set. Please run 'llmo context set organization <title>'")
-		}
-
-		p, found, err := project.FindProjectByTitle(env, value, "")
-		if err != nil {
-			return err
-		}
-		if !found {
-			return fmt.Errorf("project not found")
-		}
-		env.Config.Context.ProjectID = p.Id
-		if err := env.Config.Save(); err != nil {
-			return err
-		}
-	default:
-		return fmt.Errorf("unknown key %s", key)
 	}
+	return nil, fmt.Errorf("org of title %q not found", title)
+}
 
-	return nil
+func findProjectByID(projects []*uv1.Project, id string) (*uv1.Project, error) {
+	for _, p := range projects {
+		if p.Id == id {
+			return p, nil
+		}
+	}
+	return nil, fmt.Errorf("project of ID %q not found", id)
+}
+
+func findProjectByTitle(projects []*uv1.Project, title string) (*uv1.Project, error) {
+	for _, p := range projects {
+		if p.Title == title {
+			return p, nil
+		}
+	}
+	return nil, fmt.Errorf("project of title %q not found", title)
 }
