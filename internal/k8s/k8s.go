9f87cabb2569b9fefc654bd39e5f6a19e46ca41b
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-28 15:22:25 -0700

    Change the K8s API server path to have "/v1/sessions" (#57)

--- internal/k8s/k8s.go
+++ internal/k8s/k8s.go
@@ -1,6 +1,7 @@
 package k8s
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/llm-operator/cli/internal/runtime"
@@ -10,8 +11,7 @@ import (
 
 // NewClient creates a new Kubernetes client.
 func NewClient(env *runtime.Env) (kubernetes.Interface, error) {
-	// Remove "/v1" from the endpoint URL. This is a hack to construct the proper URL for k8s.
-	host := env.Config.EndpointURL[:len(env.Config.EndpointURL)-2]
+	host := fmt.Sprintf("%s/sessions", env.Config.EndpointURL)
 	return kubernetes.NewForConfig(&rest.Config{
 		Host:        host,
 		BearerToken: env.Token.AccessToken,
