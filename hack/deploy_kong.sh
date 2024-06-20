48b4b8ed0f5fd2f9b970c42569488f33af6ecb0a
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-06-19 20:48:48 -0700

    Add another kong for internal ingress controller
    This is used to route requests for Jupyter Notebooks.
    
    The original Kong receives a request for "/v1/sessions" and
    routes it to Session Manager Server/Agent. Then Session
    Manager Agent routes the request to this internal Kong with
    the same URL ("/v1/sessions").
    
    Originally we are planning to make Session Manager truncate
    "/v1/sessions/<cluster ID>", but that didn't work. Jupyter Notebook
    didn't work when its ServerApp.base_url is different from the
    user visible path.

--- hack/deploy_kong.sh
+++ hack/deploy_kong.sh
@@ -9,3 +9,5 @@ basedir=$(dirname "$0")
 helm repo add kong https://charts.konghq.com
 helm repo update
 helm install --create-namespace kong-proxy kong/kong -n kong --set ingressController.installCRDs=false -f "${basedir}"/kong_values.yaml
+
+helm install --create-namespace kong-proxy kong/kong -n kong-internal --set ingressController.installCRDs=false -f "${basedir}"/kong_internal_values.yaml
