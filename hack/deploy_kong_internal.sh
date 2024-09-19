35b8f582f6bf7bde43ba812264afff63b77cc6b6
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-09-19 10:56:21 -0700

    Remove kong-internal
    We no longer need this as session-manager-agent does not depend on it.

--- hack/deploy_kong_internal.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-basedir=$(dirname "$0")
-
-helm repo add kong https://charts.konghq.com
-helm repo update
-helm install --create-namespace kong-proxy kong/kong -n kong-internal --set ingressController.installCRDs=false -f "${basedir}"/kong_internal_values.yaml
