d74b01778416fd3ffc9b4fff592548b9eec59bf5
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-09-23 15:44:23 -0700

    Update llmo-demo config (#417)
    Follow the updated values.yaml definitions.

--- hack/llmo-demo/control-plane/deploy_llm_operator.sh
+++ hack/llmo-demo/control-plane/deploy_llm_operator.sh
@@ -10,6 +10,4 @@ helm upgrade \
   llm-operator \
   oci://public.ecr.aws/cloudnatix/llm-operator-charts/llm-operator \
   --set tags.worker=false \
-  -f "${basedir}"/../../llm-operator-values.yaml \
-  -f "${basedir}"/../../llm-operator-values-cpu-only.yaml \
   -f "${basedir}"/llm-operator-values.yaml
