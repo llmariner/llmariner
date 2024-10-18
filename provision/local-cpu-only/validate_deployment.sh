4f62f69d57d64c7784b3ece5d1514ba84c2ebf39
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2024-10-18 14:55:50 -0700

    test: Run a vLLM runtime in integration test

--- provision/local-cpu-only/validate_deployment.sh
+++ provision/local-cpu-only/validate_deployment.sh
@@ -8,7 +8,7 @@ kubectl wait --timeout=300s --for=condition=ready pod -n llmariner -l app.kubern
 
 echo "Deployment is ready!"
 
-echo "Waiting for the model to be loaded..."
+echo "Waiting for google-gemma-2b-it-q4_0 to be loaded..."
 
 for i in {1..300}; do
   if llma models list | grep google-gemma-2b-it-q4_0; then
@@ -17,11 +17,24 @@ for i in {1..300}; do
   sleep 1
 done
 
-echo "Model is loaded!"
+echo "Model google-gemma-2b-it-q4_0 is loaded!"
 
 echo "Running chat completion..."
 
 llma chat completions create --model google-gemma-2b-it-q4_0 --role user --completion  "What is the capital of France?"
+
+echo "Waiting for TinyLlama-TinyLlama-1.1B-Chat-v1.0-awq to be loaded..."
+
+for i in {1..300}; do
+  if llma models list | grep TinyLlama-TinyLlama-1.1B-Chat-v1.0-awq; then
+    break
+  fi
+  sleep 1
+done
+
+echo "Model TinyLlama-TinyLlama-1.1B-Chat-v1.0-awq is loaded!"
+
+llma chat completions create --model TinyLlama-TinyLlama-1.1B-Chat-v1.0-awq --role user --completion  "What is the capital of France?"
 
 echo "Chat completion is done!"
 
