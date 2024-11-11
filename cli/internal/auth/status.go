e2774f18530928ca52ed515730e57836fb3963e9
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-11-11 09:13:06 -0800

    feat(cli): display the user id in the "auth status"

--- cli/internal/auth/status.go
+++ cli/internal/auth/status.go
@@ -1,9 +1,14 @@
 package auth
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/llmariner/llmariner/cli/internal/accesstoken"
+	ihttp "github.com/llmariner/llmariner/cli/internal/http"
 	"github.com/llmariner/llmariner/cli/internal/runtime"
 	"github.com/llmariner/llmariner/cli/internal/ui"
+	uv1 "github.com/llmariner/user-manager/api/v1"
 	"github.com/spf13/cobra"
 )
 
@@ -13,16 +18,29 @@ func statusCmd() *cobra.Command {
 		Short: "Login status",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			p := ui.NewPrompter()
-
-			if _, err := runtime.NewEnv(cmd.Context()); err != nil {
-				p.Warn("Not logged in.")
-				return nil
-			}
-
-			p.Printf("Logged in.\n")
-			p.Printf("- Token file location: %s\n", accesstoken.TokenFilePath())
-			return nil
+			return status(cmd.Context())
 		},
 	}
+}
+
+func status(ctx context.Context) error {
+	p := ui.NewPrompter()
+
+	env, err := runtime.NewEnv(ctx)
+	if err != nil {
+		p.Warn("Not logged in.")
+		return nil
+	}
+
+	var req uv1.GetUserSelfRequest
+	var user uv1.User
+	if err := ihttp.NewClient(env).Send(http.MethodGet, "/users:getSelf", &req, &user); err != nil {
+		return err
+	}
+
+	p.Printf("Logged in.\n")
+	p.Printf("- ID: %s\n", user.Id)
+	p.Printf("- Token file location: %s\n", accesstoken.TokenFilePath())
+
+	return nil
 }
