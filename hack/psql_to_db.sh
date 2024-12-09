c1a7c153beb7f283f9b158647e01b3e52e5451a7
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-12-08 21:50:35 -0800

    fix(hack): fix psql_to_db.sh to work with the current postgres config (#577)

--- hack/psql_to_db.sh
+++ hack/psql_to_db.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-kubectl exec -it -n postgres deploy/postgres -- psql -h localhost -U ps_user --no-password -p 5432 -d job_manager
+kubectl exec -it -n postgres postgres-0 -- env PGPASSWORD=ps_password psql -h localhost -U ps_user -p 5432 -d job_manager
