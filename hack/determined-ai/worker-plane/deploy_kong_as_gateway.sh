b6406c2b88b7269213e5c25e5705f364fef05909
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-01-10 17:47:49 -0800

    patch kong

--- hack/determined-ai/worker-plane/deploy_kong_as_gateway.sh
+++ hack/determined-ai/worker-plane/deploy_kong_as_gateway.sh
@@ -17,3 +17,49 @@ helm repo update
 helm upgrade --install kong kong/ingress  -f ./kong_values.yaml
 
 kubectl apply -f "${basedir}"/gateway.yaml
+
+# Enable TCP request proxying.
+#
+# See https://docs.konghq.com/kubernetes-ingress-controller/latest/guides/services/tcp/#:~:text=Create%20TCP%20routing%20configuration%20for,Service%20that's%20running%20inside%20Kubernetes.
+#
+# TODO(kenji): Determined AI picks up port 50000 by default as we configure the port as the lowest available port.
+# This works for this example demo setting, but in a real-world scenario, we need to support a range of ports.
+kubectl patch deploy kong-gateway --patch '{
+  "spec": {
+    "template": {
+      "spec": {
+        "containers": [
+          {
+            "name": "proxy",
+            "env": [
+              {
+                "name": "KONG_STREAM_LISTEN",
+                "value": "0.0.0.0:50000"
+              }
+            ],
+            "ports": [
+              {
+                "containerPort": 50000,
+                "name": "det-50000",
+                "protocol": "TCP"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}'
+
+kubectl patch service kong-gateway-proxy --patch '{
+ "spec": {
+   "ports": [
+     {
+       "name": "det-50000",
+       "port": 50000,
+       "protocol": "TCP",
+       "targetPort": 50000
+     }
+   ]
+ }
+}'
