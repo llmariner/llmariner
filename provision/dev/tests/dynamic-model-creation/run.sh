4307681c7c842d84747c9decf78b5308a731c4b8
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-03-20 13:14:07 -0700

    test: add an end-to-end test for dynamic model loading

---
+++ provision/dev/tests/dynamic-model-creation/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Creating a model..."
+llma models create deepseek-r1:1.5b -s ollama
+
+# Wait until the model is loaded. The status of the model becomes "succeeded" when it is loaded.
+for i in {1..300}; do
+  if llma models list | grep deepseek-r1:1.5b | grep succeeded; then
+	break
+  fi
+  sleep 1
+done
+
+echo "Model is loaded!"
+echo "Running chat completion..."
+llma chat completions create --model deepseek-r1:1.5b --role user --completion "What is the capital of France?"
