96f7dad5058f3d413e89cfb8ead6deb5d6a26690
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-03-24 23:58:49 -0700

    test: test Hugging Face download (#761)
    Created a new GitHub secret for storing the Hugging Face API key.

---
+++ provision/dev/tests/dynamic-model-creation/test_hugging_face_download.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Creating a model..."
+llma models create TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF -s hugging-face
+
+# Wait until the model is loaded. The status of the model becomes "succeeded" when it is loaded.
+for i in {1..300}; do
+  if llma models list | grep TheBloke-TinyLlama-1.1B-Chat-v1.0-GGUF | grep succeeded; then
+	break
+  fi
+  sleep 1
+done
+
+echo "Model is loaded!"
+echo "Running chat completion..."
+llma chat completions create --model TheBloke-TinyLlama-1.1B-Chat-v1.0-GGUF --role user --completion "What is the capital of France?"
