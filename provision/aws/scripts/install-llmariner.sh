40762d0bf3f3d77f1bb6c1d5214d92b71419f61e
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-05-06 09:59:15 -0700

    fix: rename helmfile.yaml to helmfile.yaml.gotmpl
    This is required in Helmfile v1.
    
    https://helmfile.readthedocs.io/en/latest/#values-files-templates
    
    https://github.com/helmfile/helmfile/blob/main/docs/proposals/towards-1.0.md

--- provision/aws/scripts/install-llmariner.sh
+++ provision/aws/scripts/install-llmariner.sh
@@ -4,5 +4,5 @@ set -xe
 helmfile apply \
   --skip-diff-on-install \
   -l app!=fake-gpu-operator \
-  --file ./dev/helmfile.yaml \
+  --file ./dev/helmfile.yaml.gotmpl \
   --state-values-set llmariner.chartPath=../llmariner
