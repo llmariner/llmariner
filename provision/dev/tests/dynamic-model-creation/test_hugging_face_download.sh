e5d8e4f2ea1c89d6a243520daa951502415100ac
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-05-06 10:37:19 -0700

    fix: fix model loading commands

--- provision/dev/tests/dynamic-model-creation/test_hugging_face_download.sh
+++ provision/dev/tests/dynamic-model-creation/test_hugging_face_download.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo "Creating a model..."
-llma models create base TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/tinyllama-1.1b-chat-v1.0.Q2_K.gguf -s hugging-face
+llma models create base TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/tinyllama-1.1b-chat-v1.0.Q2_K.gguf --source-repository hugging-face
 
 # Wait until the model is loaded. The status of the model becomes "succeeded" when it is loaded.
 for i in {1..300}; do
@@ -15,4 +15,4 @@ done
 
 echo "Model is loaded!"
 echo "Running chat completion..."
-llma chat completions create base --model TheBloke-TinyLlama-1.1B-Chat-v1.0-GGUF-tinyllama-1.1b-chat-v1.0.Q2_K.gguf  --role user --completion "What is the capital of France?"
+llma chat completions create --model TheBloke-TinyLlama-1.1B-Chat-v1.0-GGUF-tinyllama-1.1b-chat-v1.0.Q2_K.gguf  --role user --completion "What is the capital of France?"
