8c05db285f02bc4eba19283e1f45034e6cb8c48b
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-05-30 13:19:37 -0700

    feat: bump inference-manager dep in cli

--- cli/internal/models/models.go
+++ cli/internal/models/models.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/AlecAivazis/survey/v2"
-	iv1 "github.com/llmariner/inference-manager/api/v1"
 	ihttp "github.com/llmariner/llmariner/cli/internal/http"
 	"github.com/llmariner/llmariner/cli/internal/runtime"
 	"github.com/llmariner/llmariner/cli/internal/ui"
@@ -33,8 +32,7 @@ func Cmd() *cobra.Command {
 	cmd.AddCommand(createCmd())
 	cmd.AddCommand(listCmd())
 	cmd.AddCommand(deleteCmd())
-	cmd.AddCommand(activateCmd())
-	cmd.AddCommand(deactivateCmd())
+
 	return cmd
 }
 
@@ -118,26 +116,6 @@ func deleteCmd() *cobra.Command {
 		Args: validateIDArg,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return delete(cmd.Context(), args[0])
-		},
-	}
-}
-
-func activateCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:  "activate <ID>",
-		Args: validateIDArg,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return activate(cmd.Context(), args[0])
-		},
-	}
-}
-
-func deactivateCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:  "deactivate <ID>",
-		Args: validateIDArg,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return deactivate(cmd.Context(), args[0])
 		},
 	}
 }
@@ -268,56 +246,6 @@ func delete(ctx context.Context, id string) error {
 	}
 
 	fmt.Printf("Deleted the model (ID: %q).\n", id)
-
-	return nil
-}
-
-func activate(ctx context.Context, id string) error {
-	env, err := runtime.NewEnv(ctx)
-	if err != nil {
-		return err
-	}
-
-	req := &iv1.ActivateModelRequest{
-		Id: id,
-	}
-	var resp iv1.ActivateModelResponse
-	if err := ihttp.NewClient(env).Send(http.MethodPost, fmt.Sprintf("%s/%s:activate", path, id), &req, &resp); err != nil {
-		return err
-	}
-
-	fmt.Printf("Sent the activaiton request for the model (ID: %q).\n", id)
-
-	return nil
-}
-
-func deactivate(ctx context.Context, id string) error {
-	p := ui.NewPrompter()
-	s := &survey.Confirm{
-		Message: fmt.Sprintf("Deactivate model %q?", id),
-		Default: false,
-	}
-	var ok bool
-	if err := p.Ask(s, &ok); err != nil {
-		return err
-	} else if !ok {
-		return nil
-	}
-
-	env, err := runtime.NewEnv(ctx)
-	if err != nil {
-		return err
-	}
-
-	req := &iv1.DeactivateModelRequest{
-		Id: id,
-	}
-	var resp iv1.DeactivateModelResponse
-	if err := ihttp.NewClient(env).Send(http.MethodPost, fmt.Sprintf("%s/%s:deactivate", path, id), &req, &resp); err != nil {
-		return err
-	}
-
-	fmt.Printf("Sent the deeactivation request for the model (ID: %q).\n", id)
 
 	return nil
 }
