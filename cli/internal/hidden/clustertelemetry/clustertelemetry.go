505dfb61d75efa27ad0dceafb5128ef3f7d3f704
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-07-01 09:03:56 -0700

    feat(cli): support group-by in list-cluster-snapshots (#850)

--- cli/internal/hidden/clustertelemetry/clustertelemetry.go
+++ cli/internal/hidden/clustertelemetry/clustertelemetry.go
@@ -30,21 +30,31 @@ func Cmd() *cobra.Command {
 }
 
 func listClusterSnapshotsCmd() *cobra.Command {
-	return &cobra.Command{
+	var groupByStr string
+	cmd := &cobra.Command{
 		Use:   "list-cluster-snapshots",
 		Short: "List cluster snapshots",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return listClusterSnapshots(cmd.Context())
+			groupBy, err := toGroupByEnum(groupByStr)
+			if err != nil {
+				return err
+			}
+			return listClusterSnapshots(cmd.Context(), groupBy)
 		},
 	}
+
+	cmd.Flags().StringVar(&groupByStr, "group-by", "", "Group snapshots with a specified key (optional). Either 'cluster' or 'product'.")
+	return cmd
 }
 
-func listClusterSnapshots(ctx context.Context) error {
+func listClusterSnapshots(ctx context.Context, groupBy cv1.ListClusterSnapshotsRequest_GroupBy) error {
 	env, err := runtime.NewEnv(ctx)
 	if err != nil {
 		return err
 	}
-	var req cv1.ListClusterSnapshotsRequest
+	req := cv1.ListClusterSnapshotsRequest{
+		GroupBy: groupBy,
+	}
 	var resp cv1.ListClusterSnapshotsResponse
 	if err := ihttp.NewClient(env).Send(http.MethodGet, path, &req, &resp); err != nil {
 		return err
@@ -57,4 +67,17 @@ func listClusterSnapshots(ctx context.Context) error {
 	fmt.Println(string(b))
 
 	return nil
+}
+
+func toGroupByEnum(groupByStr string) (cv1.ListClusterSnapshotsRequest_GroupBy, error) {
+	switch groupByStr {
+	case "":
+		return cv1.ListClusterSnapshotsRequest_GROUP_BY_UNSPECIFIED, nil
+	case "cluster":
+		return cv1.ListClusterSnapshotsRequest_GROUP_BY_CLUSTER, nil
+	case "product":
+		return cv1.ListClusterSnapshotsRequest_GROUP_BY_PRODUCT, nil
+	default:
+		return cv1.ListClusterSnapshotsRequest_GROUP_BY_UNSPECIFIED, fmt.Errorf("invalid repository type %q. Must be 'cluster' or 'product'", groupByStr)
+	}
 }
