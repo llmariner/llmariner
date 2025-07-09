c8777436585cd6b93b342f5dadef619c0f65e10c
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-07-09 16:36:06 -0700

    chore(hack): add a test script for audio transcription

---
+++ hack/audio/run_transcription.py
@@ -0,0 +1,23 @@
+import base64
+import os
+
+from openai import OpenAI
+
+if len(sys.argv) != 2:
+    print("Usage: python run_transcription.py <audio_file>")
+    sys.exit(1)
+
+audio_file = sys.argv[1]
+
+client = OpenAI(
+    api_key=os.getenv("LLMARINER_API_KEY"),
+    base_url="http://localhost:8080/v1",
+)
+
+model = "openai-whisper-large-v3-turbo"
+
+response = client.audio.transcriptions.create(
+    model=model,
+    file=open(audio_file, "rb")
+)
+print(response)
