3a7a9bd374bac0ff19fe5752714331716ba5d8a0
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-08-05 23:38:41 -0700

    fix: tweak validation scripts to wait for runtime pods

--- provision/dev/validate_deployment.sh
+++ provision/dev/validate_deployment.sh
@@ -19,6 +19,19 @@ done
 
 echo "Model is loaded!"
 
+echo "Waiting for the inference runtime pod is created..."
+
+for i in {1..300}; do
+  if kubectl get pod -n llmariner ollama-google-gemma-2b-it-q4-0-0; then
+    break
+  fi
+  sleep 1
+done
+
+kubectl wait --timeout=300s --for=condition=ready pod -n llmariner ollama-google-gemma-2b-it-q4-0-0
+
+echo "Inference runtime pod is ready!"
+
 echo "Running chat completion..."
 
 llma chat completions create --model google-gemma-2b-it-q4_0 --role user --completion  "What is the capital of France?"
